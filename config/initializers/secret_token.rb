de7782d1e3d85da6e375d14a1be6a6432f407059
Bollocks <jqrainwater@yahoo.com>
2014-10-04 02:22:11 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PomoGame::Application.config.secret_key_base = '8857b09d441fff9134f4cb438b349064121ed9ca476a856698fbb2f516dcf22f0a6db8376864d34068328176092f386b9d38fb57045725ff4cfdc1bfa4293190'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PomoGame::Application.config.session_store :cookie_store, key: '_pomoGame_session'
