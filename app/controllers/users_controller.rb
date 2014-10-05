acf2902c098c04e363f4aa90cac36c89a750a1d6
Bollocks <jqrainwater@yahoo.com>
2014-10-05 08:01:47 +0000

    Update create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,17 +18,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html do 
-          session[:user_id] = @user.id
-          redirect_to root_path, notice: 'User was successfully created.'
-        end
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      flash[:alert] = "Error in account creation"
+      render "welcome/index"
     end
   end
 
